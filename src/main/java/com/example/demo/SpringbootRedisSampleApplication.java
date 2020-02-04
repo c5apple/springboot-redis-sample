4289ba63e4994622d11145d3621efe2349823071
c5apple <c5apple_party@yahoo.co.jp>
2020-02-04 09:13:47 +0900

    redis

--- src/main/java/com/example/demo/SpringbootRedisSampleApplication.java
+++ src/main/java/com/example/demo/SpringbootRedisSampleApplication.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
 public class SpringbootRedisSampleApplication {
 
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootRedisSampleApplication.class, args);
+		//		SpringApplication.run(SpringbootRedisSampleApplication.class, args);
+
+		try (ConfigurableApplicationContext ctx = SpringApplication.run(SpringbootRedisSampleApplication.class, args)) {
+			SpringbootRedisSampleApplication app = ctx.getBean(SpringbootRedisSampleApplication.class);
+			app.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	public void run() {
+
+		// 文字列設定
+		stringRedisTemplate.opsForValue().set("key1", "value1");
+
+		// 文字列取得
+		System.out.println(stringRedisTemplate.opsForValue().get("key1"));
+	}
 }
